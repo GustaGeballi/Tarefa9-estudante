f9b0b5703d8b3b2f18245f0ec46d05acb911f303
GustaGeballi <gustavo.geballi@hotmail.com>
2022-05-31 23:55:15 -0300

    Tarefa 9

---
+++ src/app/estudante.ts
@@ -0,0 +1,8 @@
+export interface Estudante {
+    id: number;
+    nome: string;
+    sobrenome:string;
+    idade: number;
+    CPF: string;
+    telefone: string;
+}
---
+++ src/app/mock-students.ts
@@ -0,0 +1,57 @@
+import { Estudante } from './estudante';
+
+export const ESTUDANTES: Estudante[] = [
+    {
+        id: 1, 
+        nome: 'Gustavo', 
+        sobrenome: 'Geballi',
+        idade: 20, 
+        CPF: '46037463859', 
+        telefone: '11989802317'
+    },    {
+        id: 2, 
+        nome: 'Maicon', 
+        sobrenome: 'aleatorio',
+        idade: 20, 
+        CPF: '46037463859', 
+        telefone: '11989802317'
+    },    {
+        id: 3, 
+        nome: 'Jumento', 
+        sobrenome: 'Celestial',
+        idade: 20, 
+        CPF: '46037463859', 
+        telefone: '11989802317'
+    },    {
+        id: 4, 
+        nome: 'Pedro', 
+        sobrenome: 'Pinola',
+        idade: 20, 
+        CPF: '46037463859', 
+        telefone: '11989802317'
+    },    {
+        id: 5, 
+        nome: 'Jade', 
+        sobrenome: 'Picon',
+        idade: 20, 
+        CPF: '46037463859', 
+        telefone: '11989802317'
+    },    {
+        id: 6, 
+        nome: 'Guilherme', 
+        sobrenome: 'Arantes',
+        idade: 20, 
+        CPF: '46037463859', 
+        telefone: '11989802317'
+    },    {
+        id: 7, 
+        nome: 'Didi', 
+        sobrenome: 'Mocó',
+        idade: 20, 
+        CPF: '46037463859', 
+        telefone: '11989802317'
+    },
+    
+
+];
+
