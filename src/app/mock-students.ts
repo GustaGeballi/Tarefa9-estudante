5368a8ebbca780ef0dcc9560d129b32ec50d3c02
GustaGeballi <72327914+GustaGeballi@users.noreply.github.com>
2022-06-07 09:59:35 -0300

    Atualização

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -6,50 +6,50 @@ export const ESTUDANTES: Estudante[] = [
         nome: 'Gustavo', 
         sobrenome: 'Geballi',
         idade: 20, 
-        CPF: '46037463859', 
-        telefone: '11989802317'
+        CPF: '8876643329', 
+        telefone: '11983949074'
     },    {
         id: 2, 
         nome: 'Maicon', 
         sobrenome: 'aleatorio',
         idade: 20, 
-        CPF: '46037463859', 
-        telefone: '11989802317'
+        CPF: '8876643329', 
+        telefone: '11983949074'
     },    {
         id: 3, 
         nome: 'Jumento', 
         sobrenome: 'Celestial',
         idade: 20, 
-        CPF: '46037463859', 
-        telefone: '11989802317'
+        CPF: '8876643329', 
+        telefone: '11983949074'
     },    {
         id: 4, 
         nome: 'Pedro', 
         sobrenome: 'Pinola',
         idade: 20, 
-        CPF: '46037463859', 
-        telefone: '11989802317'
+        CPF: '8876643329', 
+        telefone: '11983949074'
     },    {
         id: 5, 
         nome: 'Jade', 
         sobrenome: 'Picon',
         idade: 20, 
-        CPF: '46037463859', 
-        telefone: '11989802317'
+        CPF: '8876643329', 
+        telefone: '11983949074'
     },    {
         id: 6, 
         nome: 'Guilherme', 
         sobrenome: 'Arantes',
         idade: 20, 
-        CPF: '46037463859', 
-        telefone: '11989802317'
+        CPF: '8876643329', 
+        telefone: '11983949074'
     },    {
         id: 7, 
         nome: 'Didi', 
         sobrenome: 'Mocó',
         idade: 20, 
-        CPF: '46037463859', 
-        telefone: '11989802317'
+        CPF: '8876643329', 
+        telefone: '11983949074'
     },
     
 
