85d80df442bf1834e165421d04a0024562563729
GustaGeballi <gustavo.geballi@hotmail.com>
2022-06-13 23:23:09 -0300

    Adicionando Professores

---
+++ src/app/mock-teachers.ts
@@ -0,0 +1,57 @@
+import { Professor } from './professor';
+
+export const PROFESSORES: Professor[] = [
+    {
+        id: 1, 
+        nome: 'Luqui', 
+        sobrenome: 'Iscai ualque',
+        idade: 60, 
+        CPF: '8876643329', 
+        telefone: '11983949074'
+    },    {
+        id: 2, 
+        nome: 'Maicu', 
+        sobrenome: 'Jéquiso',
+        idade: 72, 
+        CPF: '8876643329', 
+        telefone: '11983949074'
+    },    {
+        id: 3, 
+        nome: 'Darti', 
+        sobrenome: 'Veide',
+        idade: 900, 
+        CPF: '8876643329', 
+        telefone: '11983949074'
+    },    {
+        id: 4, 
+        nome: 'Mario', 
+        sobrenome: 'Bros',
+        idade: 64, 
+        CPF: '8876643329', 
+        telefone: '11983949074'
+    },    {
+        id: 5, 
+        nome: 'Luigi', 
+        sobrenome: 'Mario Verde',
+        idade: 63, 
+        CPF: '8876643329', 
+        telefone: '11983949074'
+    },    {
+        id: 6, 
+        nome: 'Cleiton', 
+        sobrenome: 'Gud of Uor',
+        idade: 200, 
+        CPF: '8876643329', 
+        telefone: '11983949074'
+    },    {
+        id: 7, 
+        nome: 'Batata', 
+        sobrenome: 'Ruffles',
+        idade: 80, 
+        CPF: '8876643329', 
+        telefone: '11983949074'
+    },
+    
+
+];
+
